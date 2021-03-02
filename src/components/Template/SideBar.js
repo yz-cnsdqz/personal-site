adcf21e8522160963e49c474b312eaf6a4a1c340
Yan Zhang <yan.zhang@tuebingen.mpg.de>
2021-03-02 23:17:55 +0100

    continue to adapt

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -32,7 +32,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Yan Zhang.</p>
     </section>
   </section>
 );
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,27 +5,25 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Yan Zhang's personal website."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">About me</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Do AI for People: Human behavior capturing, understanding, and synthesis.
           </p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
+        <Link to="/projects">publications</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      {/* <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p> */}
     </article>
   </Main>
 );
