0054686cb344289efa3a7bacdec04df4e9cf550e
Yan Zhang <yan.zhang@tuebingen.mpg.de>
2021-03-03 15:06:56 +0100

    update education

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,25 +3,25 @@ const degrees = [
     school: 'Ulm University',
     degree: 'PhD Computer Science',
     link: 'https://www.uni-ulm.de/en/',
-    year: 2016,
+    year: 2020,
   },
   {
     school: 'Saarland University',
     degree: 'M.S./PhD Computer Science',
     link: 'https://www.uni-saarland.de/en/home.html',
-    year: 2016,
+    year: 2015,
   },
   {
     school: 'University of Manchester',
     degree: 'M.S. Advanced Control Systems',
     link: 'https://www.manchester.ac.uk/',
-    year: 2016,
+    year: 2010,
   },
   {
     school: 'Southwest Jiaotong University',
     degree: 'B.S. Mechanical Engineering and Automation',
     link: 'https://en.swjtu.edu.cn/',
-    year: 2012,
+    year: 2009,
   },
 ];
 
